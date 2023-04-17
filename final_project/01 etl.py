173f6a1e1c63dc052deac2a4bdf4c53d84dc8b44
mnikithareddy <mnikithareddy>
2023-04-17 00:11:43 +0000

    Initial Etl

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -13,6 +13,52 @@ print("YOUR CODE HERE...")
 
 # COMMAND ----------
 
+NYC_WEATHER_FILE_PATH
+
+# COMMAND ----------
+
+# Check for files in weather data path
+import os
+dbutils.fs.ls("dbfs:/FileStore/tables/raw/weather/")
+
+
+# COMMAND ----------
+
+BIKE_TRIP_DATA_PATH
+
+# COMMAND ----------
+
+# Check for files in bike trip data path
+files = dbutils.fs.ls("dbfs:/FileStore/tables/raw/bike_trips/")
+num_files = len(files)
+print(f"Total number of bike trip files: {num_files}")
+
+# COMMAND ----------
+
+# Load the data file into a DataFrame
+df = spark.read.format("csv").option("header", True).load(NYC_WEATHER_FILE_PATH)
+
+# Print the schema of the DataFrame
+df.printSchema()
+
+# COMMAND ----------
+
+# Define schema for weather data
+schema = StructType([
+    StructField("dt", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True),
+])
+
+schema_weather = StructType([
+    StructField("dt", StringType(), True),
+    StructField("temp", StringType(), True),
+    StructField("feels_like", StringType(), True),
+    StructField("pressure", StringType(), True),
+])
+
+# COMMAND ----------
+
 import json
 
 # Return Success
