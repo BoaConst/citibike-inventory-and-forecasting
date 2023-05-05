38e58ee4d8c80bf51cf9d523537ec9f7d671d934
BoaConst <BoaConst>
2023-05-05 19:58:03 +0000

    readded parse

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -4,12 +4,12 @@
 # COMMAND ----------
 
 # DBTITLE 1,Parsing the parameters provided by the main notebook
-# start_date = str(dbutils.widgets.get('01.start_date'))
-# end_date = str(dbutils.widgets.get('02.end_date'))
-# hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-# promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
+start_date = str(dbutils.widgets.get('01.start_date'))
+end_date = str(dbutils.widgets.get('02.end_date'))
+hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
+promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
 
-# print(start_date,end_date,hours_to_forecast, promote_model)
+print(start_date,end_date,hours_to_forecast, promote_model)
 
 # COMMAND ----------
 
