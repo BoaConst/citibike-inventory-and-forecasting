1738627264240ae9b094288d43d51d482dd68f8f
BoaConst <BoaConst>
2023-05-05 01:59:27 +0000

    Adding initialize environment and ETL code to the master branch

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -3,6 +3,7 @@
 
 # COMMAND ----------
 
+# Parsing the parameters provided by the main notebook
 start_date = str(dbutils.widgets.get('01.start_date'))
 end_date = str(dbutils.widgets.get('02.end_date'))
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
@@ -12,88 +13,123 @@ print(start_date,end_date,hours_to_forecast, promote_model)
 
 # COMMAND ----------
 
-print("At the high-level, there are {} and {} no. of files containing the raw data for New York Weather Data & Bike Trip Data respectively.".format(len(dbutils.fs.ls(NYC_WEATHER_FILE_PATH)), len(dbutils.fs.ls(BIKE_TRIP_DATA_PATH))))
+# Introductory information
+print("Starting the ETL Process for New York Weather Data and Bike Trip Data.")
+
+print("At a high-level, "
+        f"there are {len(dbutils.fs.ls(NYC_WEATHER_FILE_PATH))} and "
+            f"{len(dbutils.fs.ls(BIKE_TRIP_DATA_PATH))} no. of files "
+                "containing the raw data for New York Weather Data & "
+                    "Bike Trip Data respectively.")
 
 # COMMAND ----------
 
-print("Starting the Streaming ETL Process for New York Weather Data and Bike Trip Data.")
+# Helper functions required as part of ETL
+from pyspark.sql import DataFrame
+
+def readDataFrameFromSource(path: str, file_format: str) -> DataFrame:
+    """
+    Creates a Spark DataFrame from a raw data file.
+
+    :param path: The location of the raw data file.
+    :param format: The format of the raw data file.
+    :return: A Spark DataFrame
+    """
+    # Create a static DataFrame to infer the schema from existing CSV files
+    df = spark.read.format(file_format) \
+        .option("header", "true") \
+            .option("inferSchema", "true") \
+                .load(path)
+
+    schema = df.schema
+
+    # Replace the DataFrame with the inferred schema
+    df = spark.read.format(file_format) \
+        .option("header", "true") \
+            .option("path", path) \
+                .schema(schema) \
+                    .load()
+    
+    return df
+
+def writeDataFrameToDeltaTable(df: DataFrame, delta_table_name: str):
+    """
+    Writes a Spark DataFrame into a delta table
+
+    :param df: Spark DataFrame
+    :param delta_table_name: The name of the delta table
+    """
+    delta_table_path = GROUP_DATA_PATH + delta_table_name
+    # Write the dataframe to delta table
+    df.write.format("delta") \
+        .mode("append") \
+            .option("path", delta_table_path) \
+                .saveAsTable(delta_table_name)
+    # Check if the directory is correctly created or not
+    try:
+        if len(dbutils.fs.ls(delta_table_path)) >= 1: 
+            print("Directory created : ", delta_table_path)    
+            display(dbutils.fs.ls(delta_table_path))  
+    except FileNotFoundError as e:
+        print("Oops! Directory not found:", e)
+
+def registerDeltaTablesAsTemporaryView(delta_table_path: str, temporary_view_name: str):
+    """
+    Loads a Delta Table as a temporary view 
+
+    :param delta_table_path: The path of the delta table
+    :param temporary_view_name: The name of the view
+    """
+    spark.read.format("delta") \
+        .load(delta_table_path) \
+            .createOrReplaceTempView(temporary_view_name)
 
 # COMMAND ----------
 
-# Create a static DataFrame to infer the schema from existing CSV files
-weather_df = spark.read.format("csv") \
-    .option("header", "true") \
-    .option("inferSchema", "true") \
-    .load(NYC_WEATHER_FILE_PATH)
+# ETL for Historical Weather Data
+weather_df = readDataFrameFromSource(NYC_WEATHER_FILE_PATH, "csv")
+weather_df.printSchema()
 
-schema = weather_df.schema
-
-# Create a streaming DataFrame to read data from the CSV files
-weather_df_stream = spark.readStream.format("csv") \
-    .option("header", "true") \
-    .option("path", NYC_WEATHER_FILE_PATH) \
-    .schema(schema) \
-    .load()
-
-weather_df_stream.printSchema()
+weather_delta_table_name = 'nyc_historical_weather_data'
+writeDataFrameToDeltaTable(weather_df, weather_delta_table_name)
 
 # COMMAND ----------
 
-# Check if the data stream is still streaming
-weather_df_stream.isStreaming
+# ETL for Historical Bike Trip Data
+bike_df = readDataFrameFromSource(BIKE_TRIP_DATA_PATH, "csv")
+bike_df.printSchema()
+
+bike_delta_table_name = 'nyc_historical_bike_trip_data'
+writeDataFrameToDeltaTable(bike_df, bike_delta_table_name)
 
 # COMMAND ----------
 
-# Write the dataframe to bronze delta table
-delta_table_name = 'nyc_historical_weather_data'
-weather_df_stream.write.format("delta").mode("append").option("path", GROUP_DATA_PATH + delta_table_name).saveAsTable(delta_table_name)
+# MAGIC %sql
+# MAGIC show tables from g02_db
 
 # COMMAND ----------
 
-display(dbutils.fs.ls('dbfs:/FileStore/tables/G02/nyc_historical_weather_data/'))
+# Register Delta tables For Historic Data as Temporary Views
+registerDeltaTablesAsTemporaryView(GROUP_DATA_PATH + weather_delta_table_name, 'historic_weather_trip_data_view')
+registerDeltaTablesAsTemporaryView(GROUP_DATA_PATH + bike_delta_table_name, 'historic_bike_trip_data_view')
+
+
+# Register CitiBike Bronze Data Tables as Temporary Views
+registerDeltaTablesAsTemporaryView(BRONZE_STATION_INFO_PATH, 'bronze_station_info_view')
+registerDeltaTablesAsTemporaryView(BRONZE_STATION_STATUS_PATH, 'bronze_station_status_view')
+registerDeltaTablesAsTemporaryView(BRONZE_NYC_WEATHER_PATH, 'bronze_nyc_weather_view')
 
 # COMMAND ----------
 
-# Create a static DataFrame to infer the schema from existing CSV files
-bike_df = spark.read.format("csv") \
-    .option("header", "true") \
-    .option("inferSchema", "true") \
-    .load(BIKE_TRIP_DATA_PATH)
-
-bike_schema = bike_df.schema
-
-# Create a streaming DataFrame to read data from the CSV files
-bike_df_stream = spark.readStream.format("csv") \
-    .option("header", "true") \
-    .option("path", BIKE_TRIP_DATA_PATH) \
-    .schema(bike_schema) \
-    .load()
-
-bike_df_stream.printSchema()
+# MAGIC %sql
+# MAGIC show tables from g02_db
 
 # COMMAND ----------
-
-# Write the dataframe to bronze delta table
-delta_table_name = 'nyc_historical_bike_trip_data'
-df_bike_trip_history.write.format("delta").mode("append").option("path", GROUP_DATA_PATH + delta_table_name).saveAsTable(delta_table_name)
-
-# COMMAND ----------
-
-# Check if the delta table is available at the Group data path
-display(dbutils.fs.ls('dbfs:/FileStore/tables/G02/nyc_historical_bike_trip_data/'))
-
-# COMMAND ----------
-
-# Filter the delta table for G02 station
-delta_path = "dbfs:/FileStore/tables/G02/nyc_historical_bike_trip_data/"
-
-# Register Delta table as temporary view
-spark.read.format("delta").load(delta_path).createOrReplaceTempView("nyc_historical_bike_trip_delta")
 
 # Filter data using SQL query
 filtered_df_g02 = spark.sql("""
   SELECT * 
-  FROM nyc_historical_bike_trip_delta 
+  FROM historic_bike_trip_data_view 
   WHERE start_station_name = 'West St & Chambers St'
 """)
 
