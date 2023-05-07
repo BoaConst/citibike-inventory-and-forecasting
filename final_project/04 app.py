b26b70c6fb578f7b18b148ef5971d73e118e56de
ashokreddykakunuri1999@gmail.com <ashokreddykakunuri1999@gmail.com>
2023-05-07 01:10:08 +0000

    FINAL COMMIT FOR MDL AND APP

--- final_project/04 app.py
+++ final_project/04 app.py
@@ -20,7 +20,7 @@ pip install folium
 # COMMAND ----------
 
 # DBTITLE 1,Current-timestamp when the notebook is run
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, dayofweek, to_timestamp
 
 currentdate = pd.Timestamp.now(tz='US/Eastern')
 fmt = '%Y-%m-%d %H:%M:%S'
@@ -46,7 +46,54 @@ print(staging_model)
 
 # COMMAND ----------
 
-display(dbutils.fs.ls(GROUP_DATA_PATH))
+import folium
+
+print("Assigned Station: ", GROUP_STATION_ASSIGNMENT)
+
+# Create a map centered at the given latitude and longitude
+
+latitude = 40.71754834
+longitude = -74.01322069
+pointer = folium.Map(location=[latitude, longitude], zoom_start=12)
+folium.Marker(location=[latitude, longitude], icon=folium.Icon(color='red')).add_to(pointer)
+
+# Display the map
+pointer
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+currentdate = pd.Timestamp.now(tz='US/Eastern').round(freq = 'H')
+fmt = '%Y-%m-%d %H:%M:%S'
+currentdate = currentdate.strftime(fmt) 
+print("The current timestamp is:",currentdate)
+
+# COMMAND ----------
+
+weather = spark.read.format("delta").load(GROUP_DATA_PATH + "Bronze_live_nyc_weather_data")
+display(weather)
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+weather_current = weather.filter(col("time") == currentdate)
+display(weather_current)
+
+# COMMAND ----------
+
+print(GROUP_STATION_ASSIGNMENT)
+info_df = spark.read.format("delta").load(BRONZE_STATION_INFO_PATH)
+import pyspark.sql.functions as F
+G_02_station_capacity = (info_df.filter(F.col('name')== GROUP_STATION_ASSIGNMENT)).select("capacity").collect()[0][0];
+# capacity = station_capacity["capacity"]
+print("Total Number of docks at this station : "+str(G_02_station_capacity))
+
+# COMMAND ----------
+
+display(dbutils.fs.ls(GROUP_DATA_PATH)
+)
 
 # COMMAND ----------
 
@@ -124,6 +171,66 @@ fig = px.scatter(
     trendline='ols'
 )
 fig.show()
+
+
+# COMMAND ----------
+
+##Next four hours Forecasting
+from pyspark.sql.functions import col
+weather_current = weather.filter(col("time") > currentdate)
+weather_next_four = weather_current.orderBy("dt")
+weather_next_four = weather_next_four.limit(4)
+display(weather_next_four)
+
+
+# COMMAND ----------
+
+# Create day_of_week column
+weather_next_four = weather_next_four.withColumn("day_of_week", dayofweek("time"))
+display(weather_next_four)
+
+# COMMAND ----------
+
+weather_next_four_df = weather_next_four.select(to_timestamp(col("time"), "yyyy-MM-dd HH:mm:ss").alias('ds'), col("feels_like"), col("clouds"), col("wind_speed"), col("day_of_week"))
+# weather_next_four_df = weather_next_four_df.withColumn('y', lit(0))
+display(weather_next_four_df)
+
+# Convert to pandas
+weather_next_four_pd = weather_next_four_df.toPandas()
+
+# COMMAND ----------
+
+display(predict_data_pd)
+
+# COMMAND ----------
+
+model_prod_uri = f'models:/{ARTIFACT_PATH}/production'
+model_prod = mlflow.prophet.load_model(model_prod_uri)
+
+prod_forecast = model_prod.predict(weather_next_four_pd)
+prod_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+weather_next_four_pd.ds = pd.to_datetime(weather_next_four_pd.ds)
+prod_forecast.ds = pd.to_datetime(prod_forecast.ds)
+results_forecast = prod_forecast[['ds','yhat']].merge(weather_next_four_pd,on="ds")
+display(results_forecast)
+results_forecast = spark.createDataFrame(results_forecast)
+
+
+# COMMAND ----------
+
+results = spark.createDataFrame(results)
+MODEL_INFERENCING_INFO = GROUP_DATA_PATH + "Gold_Inferencing_tables"
+
+results = spark.createDataFrame(results)
+results.write.format("delta").option("path", MODEL_INFERENCING_INFO).mode("overwrite").save()
+
+# COMMAND ----------
+
+
+MODEL_FORECASTING_INFO = GROUP_DATA_PATH + "Gold_Forecasting_tables"
+
+results_forecast.write.format("delta").option("path", MODEL_FORECASTING_INFO).mode("overwrite").save()
 
 
 # COMMAND ----------
