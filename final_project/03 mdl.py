cae43ba86419dea776f46ecc7a16e0719299ff9b
ashokreddykakunuri1999@gmail.com <ashokreddykakunuri1999@gmail.com>
2023-05-07 01:25:42 +0000

    reverting commented code

--- final_project/03 mdl.py
+++ final_project/03 mdl.py
@@ -7,14 +7,14 @@ promote_model = True
 
 # COMMAND ----------
 
-# start_date = str(dbutils.widgets.get('01.start_date'))
-# end_date = str(dbutils.widgets.get('02.end_date'))
-# hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-# promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
-# promote_model = True
+start_date = str(dbutils.widgets.get('01.start_date'))
+end_date = str(dbutils.widgets.get('02.end_date'))
+hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
+promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
+promote_model = True
 
-# print(start_date,end_date,hours_to_forecast, promote_model)
-# print("YOUR CODE HERE...")
+print(start_date,end_date,hours_to_forecast, promote_model)
+print("YOUR CODE HERE...")
 
 # COMMAND ----------
 
